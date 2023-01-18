af6ee20560e5e499d0ad1f87602b6824d3269307
Mihailo <106537730+Mihail18071983@users.noreply.github.com>
2023-01-18 23:18:36 +0200

    02-phone-book

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,58 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+export class App extends Component {
+  state = { contacts: [], name: '', number: '' };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const { contacts } = this.state;
+    console.log(e.target.elements.name.value);
+    console.log(e.target.elements.number.value);
+    const name = e.target.elements.name.value;
+    const number = e.target.elements.number.value;
+    contacts.push({ [name]: number });
+    console.dir(contacts);
+    this.setState({ contacts });
+    console.dir(this.state);
+  };
+
+  render() {
+    const {contacts}=this.state
+    return (
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name
+            <input
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+            />
+          </label>
+          <label>
+            Number
+            <input
+              type="tel"
+              name="number"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+            />
+          </label>
+          <button type="submit">Add contact</button>
+          <div>
+          <p>Contacts</p>
+          <ul>
+            {contacts.map((contact, idx) => {
+              const propName=Object.keys(contact)
+              return <li key={idx}><span>{propName}:</span><span>{contact[propName]}</span></li>;
+            })}
+          </ul>
+        </div>
+        </form>
+       
+    );
+  }
+}
