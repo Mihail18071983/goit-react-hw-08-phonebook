c62a4b60eb77c7d4b5741dbd872dd1ce3360b6eb
Mihail18071983 <106537730+Mihail18071983@users.noreply.github.com>
2023-03-08 16:17:15 +0000

    Deploying to gh-pages from @ Mihail18071983/goit-react-hw-08-phonebook@fe2698459b39eb98b7e0e1ae8cff9a140c86455c 🚀

---
+++ static/js/6.2e4bf4c5.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[6],{3006:function(e,o,n){n.r(o);var t=n(184);o.default=function(){return(0,t.jsx)("h1",{children:"Home page"})}}}]);
+//# sourceMappingURL=6.2e4bf4c5.chunk.js.map
