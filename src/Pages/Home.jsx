0dd15aaeab83c99eae26ed4a3505fbb01885adb2
Mihailo <106537730+Mihail18071983@users.noreply.github.com>
2023-03-10 20:52:46 +0200

    update

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,38 @@
+import { useAuth } from 'shared/hooks/useAuth';
+import { MainMassage } from './Home.styled';
+import { AdditionalText } from './Home.styled';
+import SmartphoneIcon from '@mui/icons-material/Smartphone';
+
 const Home = () => {
-  return <h1>Home page</h1>;
+  const { isLoggedIn } = useAuth();
+  return (
+    <>
+      <SmartphoneIcon
+        style={{
+          display:'block',
+          color: '#006400',
+          fontSize: '150px',
+          marginTop: '150px',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+        }}
+      />
+
+      {isLoggedIn ? (
+        <MainMassage>
+          Create a personal PhoneBook
+          <br /> and manage your contacts
+        </MainMassage>
+      ) : (
+        <>
+          <MainMassage>Welcome in PhoneBook application!</MainMassage>
+          <AdditionalText>
+            In order to get started, create a new account or log in to the app
+          </AdditionalText>
+        </>
+      )}
+    </>
+  );
 };
 
-export default Home
+export default Home;
---
+++ src/Pages/Home.styled.js
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled';
+
+export const MainMassage = styled.h1`
+  margin-top: 10px;
+  margin-bottom: 20px;
+  text-align: center;
+  font-size: 56px;
+  font-weight: 500;
+  letter-spacing: 1px;
+`;
+
+export const AdditionalText = styled.p`
+  text-align: center;
+  font-size: 33px;
+  color: #3f4041;
+  font-weight: 400;
+`;
