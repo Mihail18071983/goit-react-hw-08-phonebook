b47f8d9517084d522f74b33fc912d33dd513d282
Mihailo <106537730+Mihail18071983@users.noreply.github.com>
2023-03-13 12:06:22 +0200

    Update BurgerMenu.styled.js

--- src/modules/BurgerMenu/BurgerMenu.styled.js
+++ src/modules/BurgerMenu/BurgerMenu.styled.js
@@ -2,16 +2,4 @@ import styled from 'styled-components';
 
 export const BurgerBox = styled.div`
 position: relative;
-  /* @media screen and (max-width: 767px) {
-   display: flex;
-    flex-direction: column;
-    gap: 10px;
-    max-width: 200px;
-  }
-
-  @media screen and (min-width: 768px) {
-    opacity: 0;
-    visibility: hidden;
-    pointer-events: none;
-  } */
 `;
