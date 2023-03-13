d54b756d1255d3ade676cf14d1c097eb451b7afa
Mihailo <106537730+Mihail18071983@users.noreply.github.com>
2023-03-13 11:47:48 +0200

    update

--- src/modules/PrivateRoute/PrivateRoute.jsx
+++ src/modules/PrivateRoute/PrivateRoute.jsx
@@ -1,4 +1,6 @@
-import {Navigate, Outlet} from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
+
+import { ColorRing } from 'react-loader-spinner';
 
 import { useAuth } from "shared/hooks/useAuth";
 
@@ -6,7 +8,15 @@ const PrivateRoute = () => {
     const {isLoggedIn, token} = useAuth();
 
     if(!isLoggedIn && token) {
-        return <p>...Loading</p>
+        return <ColorRing
+              visible={true}
+              height="30"
+              width="30"
+              ariaLabel="blocks-loading"
+              wrapperStyle={{}}
+              wrapperClass="blocks-wrapper"
+              colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
+            />
     }
 
     if(!isLoggedIn && !token) {
--- src/modules/PublicRoute/PublicRoute.jsx
+++ src/modules/PublicRoute/PublicRoute.jsx
@@ -1,19 +1,31 @@
-import {Navigate, Outlet} from "react-router-dom";
+import { Navigate, Outlet } from 'react-router-dom';
 
-import { useAuth } from "shared/hooks/useAuth";
+import { useAuth } from 'shared/hooks/useAuth';
+
+import { ColorRing } from 'react-loader-spinner';
 
 const PublicRoute = () => {
-    const {isLoggedIn, token} = useAuth();
+  const { isLoggedIn, token } = useAuth();
 
-    if(!isLoggedIn && token) {
-        return <p>...Loading</p>
-    }
+  if (!isLoggedIn && token) {
+    return (
+      <ColorRing
+        visible={true}
+        height="30"
+        width="30"
+        ariaLabel="blocks-loading"
+        wrapperStyle={{}}
+        wrapperClass="blocks-wrapper"
+        colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
+      />
+    );
+  }
 
-    if(isLoggedIn) {
-        return <Navigate to="/contacts" />
-    }
+  if (isLoggedIn) {
+    return <Navigate to="/contacts" />;
+  }
 
-    return <Outlet />
-}
+  return <Outlet />;
+};
 
 export default PublicRoute;
